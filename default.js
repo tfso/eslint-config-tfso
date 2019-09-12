129834cb2642d9689f0e7f8d67eb15d748c076f2
lostfields <nicolai.gjaerum@gmail.com>
2019-09-12 22:35:30 +0200

    allow function()

--- default.js
+++ default.js
@@ -4,9 +4,9 @@ module.exports = {
         //"eol-last": "off",
         "no-unused-vars": "warn",
         "prefer-const": "warn",
-        "no-duplicate-imports": "error",   
-        
-        "space-before-function-paren": ["error", {"anonymous": "always", "named": "never", "asyncArrow": "always"}],
+        "no-duplicate-imports": "error",
+
+        "space-before-function-paren": ["error", {"anonymous": "never", "named": "never", "asyncArrow": "always"}],
         "semi": ["warn", "never"],
         "comma-spacing": ["error", { "before": false, "after": true }],
         "quotes": ["error", "single", { "allowTemplateLiterals": true }],
