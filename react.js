513d84ec68fd07edae101a2fa80a1a358dc92bdd
lostfields <nicolai.gjaerum@gmail.com>
2020-02-19 09:52:19 +0100

    override rules for tests

--- react.js
+++ react.js
@@ -9,5 +9,17 @@ module.exports = {
         
         "jsx-quotes": ["warn", "prefer-single"],
         "react/jsx-curly-spacing": ["warn", { "when": "never", "spacing": { "objectLiterals": "never" }, "children": { "when": "never" }, "allowMultiline": true }]
-    }
+    },
+    overrides: [
+        {
+            files: ['**/__test__/**/*.ts', '**/__test__/**/*.tsx', '**/*.test.ts', '**/*.test.tsx'],
+            rules: {
+                "@typescript-eslint/no-non-null-assertion": "off",
+                "@typescript-eslint/consistent-type-assertions": ["error", {
+                    "assertionStyle": "as",
+                    "objectLiteralTypeAssertions": "allow"
+                }]
+            }
+        }
+    ]
 }
